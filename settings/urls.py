41a46d2d140b54474ec691898c5e6004e4b2c134
vellimir <kkazancov@gmail.com>
2025-06-24 22:08:47 +0300

    install swagger

--- settings/urls.py
+++ settings/urls.py
@@ -1,10 +1,34 @@
+from typing import List
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Test task TodoList",
+        default_version="v1",
+        description="API documentation for Inno",
+    ),
+    public=True,
+)
+
+urlpatterns: List[path] = [
     path("admin/", admin.site.urls),
+    re_path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
+
+    path("", include("apps.tasks.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
